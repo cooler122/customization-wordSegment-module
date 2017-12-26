cdb406db653ed548f3ee8bfb7f0755221d20b31d
张胜 <sheng.zhang@flyingwings.cn>
2017-12-26 11:30:45 +0800

    新建分词项目

---
+++ customization-wordSegment-api/src/main/java/com/cooler/semantic/App.java
@@ -0,0 +1,13 @@
+package com.cooler.semantic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
